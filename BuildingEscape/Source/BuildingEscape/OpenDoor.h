fa6ec2b35d0c71412fadf5cf75b897bdb3f1115a
bjknight87 <bjknight87@yahoo.com>
2018-11-12 22:25:34 -0600

    Built 2nd door, updated material colors for chairs and table, and added first pressure plate. Section 3, lecture 73

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,8 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -16,14 +16,19 @@ public:
 	// Sets default values for this component's properties
 	UOpenDoor();
 
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
 
-		
+	UPROPERTY(VisibleAnywhere)
+		float OpenAngle = 90.f;
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;
 	
 };
